a47ede51072d8692b7737e81e18218ac367e5638
Preetam <preetam96@gmail.com>
2026-02-19 13:36:34 +0530

    first commit

---
+++ src/Main.java
@@ -0,0 +1,48 @@
+public class Main {
+
+    public static void main(String[] args) {
+        // check whether both the arrays are equal oe not
+//        int arr[] = {35, 26, 78, 34, 23, 67};
+//        int arr2[] = {35, 26, 78, 34, 23, 67};
+//        int count=0;
+//        if (arr.length == arr2.length) {
+//            for (int i = 0; i < arr.length; i++) {
+//                for (int j = 0; j < arr2.length; j++) {
+//                    if (i == j) {
+//                        if (arr[i] == arr2[j]) {
+//                           count++;
+//                        } else {
+//                            System.out.println("both are not equal");
+//                            break;
+//                        }
+//                    }
+//                }
+//            }
+//        }
+//        if(count==arr.length){
+//            System.out.println("both the arrays are equal");
+//        }
+
+
+        // Bubble shot
+        int arr[] = {34,45,72,67,38,92,48};
+
+        int temp = 0;
+        for(int i=0; i< arr.length-1; i++){
+            for(int j=0; j< arr.length-1; j++){
+                if(arr[j]>arr[j+1]){
+                   temp =arr[j+1];
+                   arr[j+1]= arr[j];
+                   arr[j]= temp;
+                }
+            }
+
+        }
+        for(int a: arr) {
+            System.out.println(a);
+        }
+
+    }
+}
+
+
